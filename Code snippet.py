9c5fe734ec55271f3b2ecc0968bb06f2a3b40219
Shibil PK <59109749+shibilpk@users.noreply.github.com>
2020-06-29 08:01:46 +0530

    Update Code snippet.py

--- Code snippet.py
+++ Code snippet.py
@@ -1,18 +1,22 @@
-def custom_titled_filter(title):
-    class Wrapper(admin.FieldListFilter):
-        def __new__(cls, *args, **kwargs):
-            instance = admin.FieldListFilter.create(*args, **kwargs)
-            instance.title = title
-            return instance
-    return Wrapper
 
-class RegistrationAdmin(admin.ModelAdmin):
-	list_display = ('id','name','email','phone','dob','message')
-	ordering = ('name',)
-	
-	list_filter = (
-    ('name', custom_titled_filter('Name')),
-    'email',
-	)
-	
-admin.site.register(Registration,RegistrationAdmin)
+#FOr Filter with Custom name ,in admin.py 
+{{{{
+	def custom_titled_filter(title):
+	    class Wrapper(admin.FieldListFilter):
+		def __new__(cls, *args, **kwargs):
+		    instance = admin.FieldListFilter.create(*args, **kwargs)
+		    instance.title = title
+		    return instance
+	    return Wrapper
+
+	class RegistrationAdmin(admin.ModelAdmin):
+		list_display = ('id','name','email','phone','dob','message')
+		ordering = ('name',)
+
+		list_filter = (
+	    ('name', custom_titled_filter('Name')),
+	    'email',
+		)
+
+	admin.site.register(Registration,RegistrationAdmin)
+}}}}
